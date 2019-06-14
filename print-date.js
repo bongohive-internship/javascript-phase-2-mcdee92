831780678c90b80ab4c2c67ebbd04795ce1eb0e9
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-14 18:51:45 +0200

    update print-date.js

--- print-date.js
+++ print-date.js
@@ -1,2 +1,8 @@
-// add a printDate function in here
-// eg: console.log("Hello World")
+function printDate() {
+    var now = new Date();
+    var currentDate = now.toDateString();
+    var currentTime = now.getHours() + ':' + now.getMinutes();
+    console.log('The current date is ' + currentDate + ' and the current time is ' + currentTime);
+}
+
+printDate();
