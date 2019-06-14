d239fe70b3bff8a068a182a2cdd8c9f453dad93c
mcdee92 <40967802+mcdee92@users.noreply.github.com>
2019-06-14 22:39:15 +0200

    add output screenshots

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,12 @@
-// calcCircumfrence function should be here
+function calcCircumference(radius) {
+    var circumference = Math.round((2 * Math.PI * radius) * 100) / 100; //round to 2 decimal places
+    var area = Math.round((Math.PI * radius ** 2) * 100) / 100;
+
+    console.log('The circumference is ' + circumference + ' and the area is ' + area);
+}
+
+calcCircumference(2);
+
+calcCircumference(7);
+
+calcCircumference(20);
--- tell-fortune.js
+++ tell-fortune.js
@@ -1,1 +1,11 @@
-// tellFortune function should be here
+function tellFortune(numOfChildren, partnerName, geoLocation, jobTitle) {
+    console.log('You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' + partnerName + ' with ' + numOfChildren + ' kids.');
+}
+
+tellFortune(2, 'Mary', 'Lusaka', 'Programmer');
+
+tellFortune(4, 'Lisa', 'Seoul', 'Doctor');
+
+tellFortune(0, 'Naomi', 'New York', 'Architect');
+
+
